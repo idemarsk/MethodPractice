7136db28e1cd3a1c1e2b91ce8b35ea2fdb7a7a2d
IrynaDelimarska <delimarskaya19@gmail.com>
2025-09-30 08:49:36 -0700

    first commit

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        //call the sayHello method
+        sayHello();
+        sayGoodbye();
+
+
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye");
+    }
+
+
+}
